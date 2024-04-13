dcb9306ec99993c38389cb52fa5595897094f4d7
paolo-gajo <paolo.gajo2@unibo.it>
2024-04-13 12:49:11 +0000

    add bash script for multiple languages

--- src/translate/nllb/launch.sh
+++ src/translate/nllb/launch.sh
@@ -8,7 +8,25 @@
 #SBATCH --output=/home/pgajo/checkthat24/.slurm/%j_output.log
 #SBATCH --error=/home/pgajo/checkthat24/.slurm/%j_error.log
 
-python /home/pgajo/checkthat24/checkthat24_DIT/src/translate/nllb/translate_nllb.py \
+declare -a tgt_languages=(
+# 'ita_Latn'
+'rus_Cyrl'
+'spa_Latn'
+'arb_Arab'
+'por_Latn'
+'slv_Latn'
+'bul_Cyrl'
+# 'ell_Grek'
+# 'kat_Geor'
+# 'pol_Latn'
+# 'deu_Latn'
+# 'fra_Latn'
+)
+
+for tgt_language in "${tgt_languages[@]}";
+do
+    python /home/pgajo/checkthat24/checkthat24_DIT/src/translate/nllb/translate_nllb.py \
     --model_name facebook/nllb-200-3.3B \
     --src_lang eng_Latn \
-    --tgt_lang ita_Latn \
+    --tgt_lang $tgt_language
+done
