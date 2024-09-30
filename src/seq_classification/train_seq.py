953dee7bce0b50e05b5d275b0ec7456e693e3db7
Luca Giordano <giordanoluca.me@gmail.com>
2024-09-30 13:52:18 +0000

    .

--- src/seq_classification/train_seq.py
+++ src/seq_classification/train_seq.py
@@ -1,3 +1,6 @@
+import os,sys 
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 import json
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, DataCollatorWithPadding
 from torch.utils.data import DataLoader
